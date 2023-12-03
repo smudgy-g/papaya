d6a198194beeb0b79482c47a532a25bfd3ee048d
smudgy-g <adamgriff86@gmail.com>
2023-12-03 15:56:13 +0100

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,41 +1,30 @@
-import DeployButton from '../components/DeployButton'
+import Button from '../components/shared/Button'
 import AuthButton from '../components/AuthButton'
 import { createClient } from '@/utils/supabase/server'
-import ConnectSupabaseSteps from '@/components/ConnectSupabaseSteps'
-import SignUpUserSteps from '@/components/SignUpUserSteps'
 import Header from '@/components/Header'
 import { cookies } from 'next/headers'
 
 export default async function Index() {
   const cookieStore = cookies()
 
-  const canInitSupabaseClient = () => {
-    // This function is just for the interactive tutorial.
-    // Feel free to remove it once you have Supabase connected.
-    try {
-      createClient(cookieStore)
-      return true
-    } catch (e) {
-      return false
-    }
-  }
 
-  const isSupabaseConnected = canInitSupabaseClient()
 
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
-      <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-        <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-          <DeployButton />
-          {isSupabaseConnected && <AuthButton />}
+      {/* <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+        <div className="w-full max-w-4xl flex justify-end items-center p-3 text-sm">
+          {<AuthButton />}
         </div>
-      </nav>
+      </nav> */}
 
       <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
         <Header />
         <main className="flex-1 flex flex-col gap-6">
-          <h2 className="font-bold text-4xl mb-4">Next steps</h2>
-          {isSupabaseConnected ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
+          <h2 className="font-bold text-4xl mb-4">Welcome to Papaya!</h2>
+          {/* <Button>
+            <span>Create a new event</span>
+          </Button> */}
+          {<AuthButton />}
         </main>
       </div>
 
@@ -43,12 +32,12 @@ export default async function Index() {
         <p>
           Powered by{' '}
           <a
-            href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
+            href="https://adamgriff.dev"
             target="_blank"
             className="font-bold hover:underline"
             rel="noreferrer"
           >
-            Supabase
+            Enthusiasm & Curiosity
           </a>
         </p>
       </footer>
